b25b533941b122a39d3cacc07b629f2f679c2853
Botinho12 <botivinicios77@gmail.com>
2024-10-25 20:32:34 -0300

    4 Commit - Views e Controllers

--- obj/Debug/net8.0/auebaGaleria.AssemblyInfo.cs
+++ obj/Debug/net8.0/auebaGaleria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("auebaGaleria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb233556c57c294ed45a6619850b6198c8045b96")]
 [assembly: System.Reflection.AssemblyProductAttribute("auebaGaleria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("auebaGaleria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
